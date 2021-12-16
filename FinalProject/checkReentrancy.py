9b50866078d309f47ba3dde1ffab79924d01759c
BonnyWang <bonny.y.gardy@gmail.com>
2021-12-16 01:35:46 -0500

    added functions

--- FinalProject/checkReentrancy.py
+++ FinalProject/checkReentrancy.py
@@ -1,3 +1,5 @@
+import nltk;
+
 def detectCallInvocation(lines):
     callLineIndexs = [];
     containsCall = False;
@@ -19,30 +21,33 @@ def detectBalanceDeduction(lines, callLineIndexs):
     # balanceDeducted = True;
 
     for lineIndex in callLineIndexs:
-        while lineIndex > 0:
-            lineIndex -= 1;
-            line = lines[lineIndex];
+        tempLineIndex = lineIndex;
+        while tempLineIndex > 0:
+            tempLineIndex -= 1;
+            line = lines[tempLineIndex];
 
             if ("alances[msg.sender]" in line) or ("alance[msg.sender]" in line):
                 if ('0' in line) or ('-' in line):
-                    
+                    callLineIndexs.remove(lineIndex);
                     # escape since this call is safe, check next call function
                     break;
             
             # Scope level check
             if ("{" in line) and ("}" not in line):
-                return False;
+                return False, callLineIndexs;
             
             # Function level check
             if "function" in line:
-                return False;
+                return False, callLineIndexs;
     
     # All call are checked with coexist of deduction inside the scope
-    return True;
+    return True, callLineIndexs;
 
 def detectModifier(lines, callLineIndexs):
     for lineIndex in callLineIndexs:
         modifierName = "onlyOwner";
+
+        updatedModifier = False;
 
         tempIndex = lineIndex;
 
@@ -52,26 +57,57 @@ def detectModifier(lines, callLineIndexs):
 
             if ("function" in line) and (modifierName in line):
                 # Safe check next call function
+                callLineIndexs.remove(lineIndex);
                 break;
             
-            if "require(msg.sender==owner)" in line.replace(" ", ""):
+            if ("require(msg.sender==owner)" in line.replace(" ", "")) and (not updatedModifier):
                 modifierIndex = tempIndex;
                 while modifierIndex > 0:
                     modifierIndex -= 1;
                     subline = lines[modifierIndex];
                     if "modifier" in subline:
-                        modifierName = subline.replace("modifier","").replace("()","").replace("{", "").replace(" ", "").strip();
+                        modifierName = nltk.word_tokenize(subline)[1];
                         
                         # recheck all
                         tempIndex = lineIndex;
+                        updatedModifier = True;
 
         if tempIndex == 0:
-            return False;
-        return True;
+            return False, callLineIndexs;
+        return True,callLineIndexs;
     
     # All the call fucntion has the corresponding modifier
-    return False;
+    return False,callLineIndexs;
 
+# find the function given current index
+def findFunction(lines, index):
+    while index > 0:
+        index -= 1;
+
+        if "function" in lines[index]:
+            return nltk.word_tokenize(lines[index])[1], index;
+
+def findVulnerFunction(lines, callLineIndexs):
+    functions = [];
+    functionLineIndex = [];
+    for lineIndex in callLineIndexs:
+        f , i =findFunction(lines, lineIndex);
+        functions.append(f);
+        functionLineIndex.append(i);
+    
+    for index,line in enumerate(lines):
+        for f in functions:
+            if (str(f+"(") in line) and (index not in functionLineIndex):
+                f, i = findFunction(lines, index);
+                functions.append(f);
+    
+    return functions;
+                
+
+                
+
+
+    
 
 
 # Check Reentrancy vulnerbility
@@ -79,15 +115,18 @@ def checkReentrancy(lines):
 
     containsCall, callLineIndexs =  detectCallInvocation(lines);
 
+    relatedFunctions = [];
+
     if containsCall:
-        balanceDeduced = detectBalanceDeduction(lines, callLineIndexs);
-        modifierPresented = detectModifier(lines, callLineIndexs);
+        balanceDeduced, callLineIndexs = detectBalanceDeduction(lines, callLineIndexs);
+        modifierPresented, callLineIndexs = detectModifier(lines, callLineIndexs);
         
         if (not balanceDeduced) and (not modifierPresented):
-            return True;
-        return False;
+            relatedFunctions = findVulnerFunction(lines, callLineIndexs);
+            return True, callLineIndexs, relatedFunctions;
+        return False, callLineIndexs, relatedFunctions;
     else:
-        return False;
+        return False, callLineIndexs, relatedFunctions;
 
 if __name__ == "__main__":
     contract = input("Enter the smart contract to check:");
@@ -97,8 +136,3 @@ if __name__ == "__main__":
     lines = fd.readlines();
 
     print(checkReentrancy(lines));
-
-
-
-
-# TODO: check cross function vulnerbility
--- FinalProject/sourcecode/1430.sol
+++ FinalProject/sourcecode/1430.sol
@@ -1,6 +1,11 @@
 
 contract FsTKerWallet {
 
+
+  function hi(){
+    callContract();
+  }
+
   function callContract(address to, bytes data) public payable returns (bool) {
     require(to.call.value(msg.value)(data));
     return true;
